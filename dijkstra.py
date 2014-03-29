12dd332870f5424d3ec75efacdce945a2ed4e364
zceee12 <richardclose@gmail.com>
2014-03-29 23:04:40 +0000

    refactor

--- dijkstra.py
+++ dijkstra.py
@@ -1,23 +1,20 @@
 import string
 import copy
 
+
 class Vertex(object):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, obj=None):
+        self.obj = obj
         self.neighbours = {}
 
-    def to_path(self, name, weighting):
-        self.neighbours[name] = weighting
-
-class Graph(object):
-    def __init__(self, vertices):
-        self.vertices = vertices
+    def to_path(self, key, weighting):
+        self.neighbours[key] = weighting
 
 
 def main():
     vertices = {}
     for letter in string.lowercase[:6]:
-        vertices[letter] = Vertex(letter)
+        vertices[letter] = Vertex()
 
     """
     a - 8 - b - 3 - c
@@ -27,21 +24,18 @@ def main():
     d - 1 - e - 2 - f
     """
 
-    graph = Graph(vertices)
+    add_undirected_edge(vertices, "a",  "b",  8)
+    add_undirected_edge(vertices, "b",  "c",  3)
+    add_undirected_edge(vertices, "a",  "d",  1)
+    add_undirected_edge(vertices, "d",  "b",  4)
+    add_undirected_edge(vertices, "d",  "e",  1)
+    add_undirected_edge(vertices, "e",  "b",  1)
+    add_undirected_edge(vertices, "e",  "f",  2)
+    add_undirected_edge(vertices, "b",  "f",  1)
+    add_undirected_edge(vertices, "c",  "f",  2)
 
-    add_undirected_edge(vertices["a"], vertices["b"], 8),
-    add_undirected_edge(vertices["b"], vertices["c"], 3),
-    add_undirected_edge(vertices["a"], vertices["d"], 1),
-    add_undirected_edge(vertices["d"], vertices["b"], 4),
-    add_undirected_edge(vertices["d"], vertices["e"], 1),
-    add_undirected_edge(vertices["e"], vertices["b"], 1),
-    add_undirected_edge(vertices["e"], vertices["f"], 2)
-    add_undirected_edge(vertices["b"], vertices["f"], 1)
-    add_undirected_edge(vertices["c"], vertices["f"], 2)
+    distances, previous = dijkstra(vertices, "a")
 
-    distances, previous = dijkstra(graph, "a")
-    print "\n\n\n\n", distances
-    print "\n\n", previous
     def get_route(previous, source, destination):
         pointer = destination
         route = []
@@ -56,39 +50,41 @@ def main():
 
 
 def dijkstra(graph, source):
-    distances = {}
-    for vertex in graph.vertices:
-        distances = dict.fromkeys(
-            graph.vertices.keys(), float("inf"))
-        previous = dict.fromkeys(
-            graph.vertices.keys())
+    distance = {}
+    for vertex in graph:
+        distances = dict.fromkeys(graph.keys(), float("inf"))
+        previous = dict.fromkeys(graph.keys())
 
     distances[source] = 0
-    q = copy.deepcopy(graph.vertices)
+    q = copy.deepcopy(graph)
 
     while(q.items()):
-        min_distance = float("inf")
-        for key, vertex in q.iteritems():
-            if distances[key] < min_distance:
-                u, min_distance = key, distances[key]
-        # if not infinity or none in our case
+        u, min_distance = get_shortest_path(q, distances)
         if min_distance == float("inf"):
             break
-
-        # remove this key from the queue
         del q[u]
 
-        # Loop over each neighbour of 
-        for name, distance in graph.vertices[u].neighbours.iteritems():
+        # Loop over each neighbour of u
+        for key, distance in graph[u].neighbours.iteritems():
             alt = min_distance + distance
-            if alt < distances[name]:
-                distances[name] = alt
-                previous[name] = u
+            if alt < distances[key]:
+                distances[key] = alt
+                previous[key] = u
     return distances, previous
 
-def add_undirected_edge(a, b, weighting):
-    a.to_path(b.name, weighting)
-    b.to_path(a.name, weighting)
+
+def add_undirected_edge(vertices, a, b, weighting):
+    vertices[a].to_path(b, weighting)
+    vertices[b].to_path(a, weighting)
+
+
+def get_shortest_path(q, distances):
+    min_distance = float("inf")
+    for key, vertex in q.iteritems():
+        if distances[key] < min_distance:
+            u, min_distance = key, distances[key]
+    return u, min_distance
+
 
 if __name__ == "__main__":
     main()
